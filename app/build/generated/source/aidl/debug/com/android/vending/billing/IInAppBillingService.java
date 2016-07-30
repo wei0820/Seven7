2139718ea0f3d831f72f7efe29ee5a686dc8b4f6
JackPan <JackPan@jackpan-MacBook-Pro.local>
2016-07-30 16:32:22 +0800

    no message

--- app/build/generated/source/aidl/debug/com/android/vending/billing/IInAppBillingService.java
+++ app/build/generated/source/aidl/debug/com/android/vending/billing/IInAppBillingService.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/jackpan/Documents/app/Thunderstormsdivisiononehundredsign/app/src/main/aidl/com/android/vending/billing/IInAppBillingService.aidl
+ * Original file: /Users/jackpan/Seven7/app/src/main/aidl/com/android/vending/billing/IInAppBillingService.aidl
  */
 package com.android.vending.billing;
 /**
